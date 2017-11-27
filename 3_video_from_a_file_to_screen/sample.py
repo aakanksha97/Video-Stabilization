433b7545c0416a4dd3224d7cecc85e542fa6e302
Saketh Katari <katari15045@iiitd.ac.in>
2017-11-27 22:22:18 +0530

    Adds 3_video_from_a_file_to_screen

--- 3_video_from_a_file_to_screen/sample.py
+++ 3_video_from_a_file_to_screen/sample.py
@@ -1,20 +1,20 @@
-# Tutorial -> https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_gui/py_video_display/py_video_display.html#capture-video-from-camera
+# Tutorial -> https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_gui/py_video_display/py_video_display.html#playing-video-from-file
 
 import cv2
 
-# VideoCapture(<0 for 1st Webcam, 1 for 2nd webcam and so on...>)
-capture = cv2.VideoCapture(0)
+capture = cv2.VideoCapture("input.avi")
 
-while (capture.isOpened() == True):
+while(capture.isOpened() == True):
 	# a, b = read() where 'a' is a boolean; it is true if there is a frame; 'b' is a frame
 	is_frame, frame = capture.read()
 
 	if( is_frame == True ):	
 		#gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		cv2.imshow('Video', frame)
+		cv2.imshow('footage from file', frame)
 		if(cv2.waitKey(1) == ord('q')):
 			break
 	else:
 		break
+
 capture.release()
 cv2.destroyAllWindows()
