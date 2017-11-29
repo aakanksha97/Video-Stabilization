c19e9825e92b0545447ec5341e222a867860be5d
Saketh Katari <katari15045@iiitd.ac.in>
2017-11-29 15:44:08 +0530

    removed some bugs Unable to track the points correctly b/w 2 images

--- code/misc/4_low_level_stabilization/sample.py
+++ code/misc/4_low_level_stabilization/sample.py
@@ -5,14 +5,13 @@ import time
 min_intensity = 0
 max_intensity = 255
 
-def store_translated_image():
-	org_img = cv2.imread("cameraman.jpg", 0)
-	cv2.imshow("original", org_img)
+def store_translated_image(filename):
+	org_img = cv2.imread(filename, 0)
 
 	rows, cols = org_img.shape
 	affine_matrix = numpy.float32([[1,0,100],[0,1,50]])
 	translated_img = cv2.warpAffine(org_img,affine_matrix,(cols,rows))
-	cv2.imwrite("cameraman_translated.jpg", translated_img)
+	cv2.imwrite("translated.jpg", translated_img)
 
 def remove_missed_points(status, prev_points, cur_points):
         prev_points_filt = []
@@ -33,12 +32,15 @@ def remove_missed_points(status, prev_points, cur_points):
                         *       #       *
                         *       *       *
 '''
-def visualize_points(image, points):
+def visualize_points(image, points, filename):
 	threshold = 127
 	(rows, cols) = image.shape
 	for item in points:
 		(row, col) = (int(item[0][0]), int(item[0][1]))
-		
+	
+		if(row < 0 or row > (rows-1) or col < 0 or col > (cols-1)):
+			continue
+	
 		if((row-1) >= 0 and (col-1) >= 0 and image[row-1][col-1] > threshold ):
 			image[row-1][col-1] = min_intensity
 		elif((row-1) >= 0 and (col-1) >= 0):
@@ -80,16 +82,15 @@ def visualize_points(image, points):
                 elif((col-1) >= 0):
                         image[row][col-1] = max_intensity		
 
-		cv2.imwrite("cameraman_points.jpg", image)
+	cv2.imwrite(filename, image)
 
 
 def detect_edges(img_org):
 	img_edg = cv2.Canny(img_org, 100, 200)
-	print("img_edg : " + str(img_edg))
-	cv2.imwrite("cameraman_edges.jpg", img_edg)
+	cv2.imwrite("edges.jpg", img_edg)
 	return img_edg
 
-def get_edge_points(img_org):
+def get_edge_points(img_org, filename):
 	img_edg = detect_edges(img_org)
 	edge_points = []
 	row_index = 0
@@ -100,16 +101,11 @@ def get_edge_points(img_org):
 				edge_points.append([[row_index, col_index]])
 			col_index = col_index + 1
 		row_index = row_index + 1
-	visualize_points(img_org, edge_points)
+	visualize_points(img_org, edge_points, filename)
 	return numpy.array(edge_points)	
 
 def main():
-
-	# params for ShiTomasi corner detection
-	feature_params = dict( maxCorners = 100,
-        	               qualityLevel = 0.3,
-                	       minDistance = 7,
-	                       blockSize = 7 )
+	store_translated_image("cameraman.jpg")
 
 	# Parameters for lucas kanade optical flow
 	lk_params = dict( winSize  = (15,15),
@@ -118,32 +114,26 @@ def main():
 
 
 	prev_frame = cv2.imread("cameraman.jpg", 0)
-	cur_frame = cv2.imread("cameraman_translated.jpg", 0)
-	prev_frame.astype(numpy.float32)
-	cur_frame.astype(numpy.float32)
+	cur_frame = cv2.imread("translated.jpg", 0)
 	
-	edge_points = get_edge_points(prev_frame)
-	print("edge_points : " + str(edge_points))
-	exit(0)
-
-	# To detect corner points
-	prev_points = cv2.goodFeaturesToTrack(prev_frame, mask = None, **feature_params)
-	prev_points.astype(numpy.float32)
-
-	# Tracks the corner points
-	cur_points, status, error = cv2.calcOpticalFlowPyrLK(prev_frame, cur_frame, prev_points, None, **lk_params)
+	prev_points = get_edge_points(prev_frame, "points_prev_frame.jpg")
+	prev_points = prev_points.astype(numpy.float32)
+	
+	# Tracks the edge points
+	cur_points, status, error = cv2.calcOpticalFlowPyrLK(prev_frame, cur_frame, prev_points, None, **lk_params)	
 
 	prev_points_updated, cur_points = remove_missed_points(status, prev_points, cur_points)
-	cur_points.astype(numpy.float32)
-	prev_points_updated.astype(numpy.float32)
+	cur_points = cur_points.astype(numpy.float32)
+	prev_points_updated = prev_points_updated.astype(numpy.float32)	
+	visualize_points(cur_frame, cur_points, "points_cur_frame.jpg")
 
-	trans_mat = cv2.findHomography(cur_points, prev_points_updated, method=cv2.LMEDS)
+	trans_mat = cv2.findHomography(cur_points, prev_points_updated)
 	trans_mat = trans_mat[0]
 
 	transformed_frame = numpy.empty(cur_frame.shape)
 	transformed_frame = cv2.warpPerspective(src=cur_frame, dst=transformed_frame, M=trans_mat, dsize=(cur_frame.shape[1], cur_frame.shape[0]))
 
-	cv2.imwrite("cameraman_predicted.jpg", transformed_frame)
+	cv2.imwrite("predicted.jpg", transformed_frame)
 
 start_time = time.time()
 main()
